9967a58584521cac98f97ba2d831c68b7f3917dc
Joshua Olson <joshua.olson@gmail.com>
2019-10-13 14:27:33 -0700

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Joshua Olson on 10/13/19.
+//  Copyright © 2019 solarmist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
